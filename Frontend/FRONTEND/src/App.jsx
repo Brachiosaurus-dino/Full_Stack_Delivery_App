fb46fb430d493cd9e6a58075235189caaa743a47
mantiss <maniidon7890@gmail.com>
2025-08-26 12:23:58 +0530

    frontend

---
+++ Frontend/FRONTEND/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="flex h-screen justify-center items-center">
+      <h1 className="text-center text-6xl ">Hello This Is Frontend</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Full_Stack_Delivery_App
+
+- npm start --> To start Server
+- npm run dev --> To start Appliciation
