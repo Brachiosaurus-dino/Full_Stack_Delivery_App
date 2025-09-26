fad9833ab53524ade9ae9e28d09461cc4e49cb53
mantiss <maniidon7890@gmail.com>
2025-09-27 01:26:06 +0530

    final

--- Backend/Config/mongo.js
+++ Backend/Config/mongo.js
@@ -1,1 +1,20 @@
 // Connection of server and database
+// mongo.js
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const connect_mongo_db=async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_DB)
+        console.log("Mongo_DB server connected")
+        
+
+        
+    }catch(error){
+        console.error("Mongo_db not connected",error.messsage)
+        process.exit(1)
+    }
+    
+}
