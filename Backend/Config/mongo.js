3caa89c91d4acb60fd8f5e2c578c00e494f05f8b
mantiss <maniidon7890@gmail.com>
2025-09-28 06:34:33 +0530

    databsese_connected

--- Backend/Config/mongo.js
+++ Backend/Config/mongo.js
@@ -18,3 +18,4 @@ export const connect_mongo_db=async () => {
     }
     
 }
+
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { connect_mongo_db } from './Config/mongo.js'
-
+import { connect_mysql_db,pool } from './Config/mysql.js'
 dotenv.config()
 
 const app = express()
@@ -10,9 +10,10 @@ const PORT=process.env.PORT
 
 app.use(express.json())
 
-
-
 await connect_mongo_db()
+console.log('MONGO DATABASE CONNECTED.....')
+await connect_mysql_db();
+console.log('MYSQL DATABASE CONNECTED.....')
 
 app.get("/",(req, res) => {
     res.send("<h1> HELLO THE SERVER IS RUNNING.....</h1>")
@@ -20,5 +21,9 @@ app.get("/",(req, res) => {
 
 app.listen(PORT, () => {
     
+    
     console.log(`Hello The Server Is Running On PORT Number:http://localhost:${PORT}`)
+    console.log('MONGO DB , MYSQL , EXPRESS.JS  EVERTHING CONNECTED WOOHOOOO............')
 })
+
+
