4adff4207a01126c3db9c7fca5bfe54037406f50
mantiss <maniidon7890@gmail.com>
2025-09-30 18:25:29 +0530

    database

--- Backend/Models/Menu_Item_Model.js
+++ Backend/Models/Menu_Item_Model.js
@@ -1,1 +1,51 @@
 // Menu Model on MongoDB
+
+//This is model Schema of Menu item Blueprint
+
+import mongoose from "mongoose";
+
+const menuSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trime: true
+        },
+        description: {
+            type: String,
+            required: true,
+            trime: true
+        },
+        price: {
+            type: Number,
+            required: true,
+            trime: true,
+            min: 0
+        },
+        category: {
+            type: String,
+            enum: ['starter', 'main', 'desert', 'drink'],
+            default: 'main',
+        },
+        available: {
+            type: Boolean,
+            default: true
+        },
+        ingredients: {
+            type: [String], //Store multiple onion , tomato , peas , cheese
+            default: [],
+        }
+
+
+    },
+    {
+        timestamps: true
+    }
+
+
+)
+
+//Menu is the sructure of table and 'Menu' name of table 
+
+export const Menu = mongoose.menuSchema('Menu', menuSchema)
+
--- Backend/Models/User_Model.js
+++ Backend/Models/User_Model.js
@@ -1,2 +1,50 @@
-// User Model on POstgres
+// User Model on Mysql
 
+//This is model Schema of User login and register Blueprint
+
+import { Datatypes, STRING, UUID } from 'sequelize'
+import { Seq } from '../Config/mysql'
+import bcrypt from  'bcryptjs'
+
+//This is the schema of our project 
+// UUID is the store unique values , UUIDV4 is version 4 creates unique values 
+
+export const User = Seq.define("User",{
+    id:{
+        type:UUID,
+        defaultValue:Datatypes.UUIDV4,
+        primaryKey:true
+    },
+    name:{
+        type:Datatypes.STRING,
+        allowNull:false,
+
+    },
+    email:{
+        type:Datatypes.STRING,
+        allowNull:false,
+        unique:true,
+        validate:{isEmail:true}
+    },
+    password:{
+        type:Datatypes.STRING,
+        allowNull:false
+    },
+    
+},
+{
+    timestamps:true,
+    tableName:"users"
+})
+
+//here before storing the object it runs beforecreate and hash th password
+
+User.beforeCreate(async (user) => {
+    user.password=await bcrypt.hash(user.password , 10)
+});
+
+//here when eevr user login this will runs to check password
+
+User.prototype.matchPassword = async (enteredPassword) =>{
+    return await bcrypt.compare(enteredPassword , this.password)
+}
