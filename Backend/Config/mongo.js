3dc877913fd91ff5bc31f01b5b418ad0cb100a9c
mantiss <maniidon7890@gmail.com>
2025-10-02 19:33:07 +0530

    checking_done

--- Backend/Config/mongo.js
+++ Backend/Config/mongo.js
@@ -8,23 +8,23 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const connect_mongo_db=async () => {
-    try{
+export const connect_mongo_db = async () => {
+    try {
         await mongoose.connect(process.env.MONGO_DB)
         console.log("Mongo_DB server connected from MONGO_DB file")
         // When i have the data i will run this file
 
-        const orders=await Order.find().limit(8)  
-        console.log('ALREADY PLACED ORDER',orders)
-        const menu=await Menu.find().limit(8)  
-        
-        console.log('Menu Items',menu)
+        const orders = await Order.find().limit(8)
+        console.log('ALREADY PLACED ORDER', orders)
+        const menu = await Menu.find().limit(8)
 
-        
-    }catch(error){
-        console.error("Mongo_db not connected",error.messsage)
+        console.log('Menu Items', menu)
+
+
+    } catch (error) {
+        console.error("Mongo_db not connected", error.messsage)
         process.exit(1)
     }
-    
+
 }
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { deleteItem , updateItem  , getallItemsbyId  , createnewItem ,getItems  } from './Controller/Menu_Controller.js'
+import { deleteItem, updateItem, getallItemsbyId, createnewItem, getItems } from './Controller/Menu_Controller.js'
 import dotenv from 'dotenv'
 import { connect_mongo_db } from './Config/mongo.js'
 import { Connection } from './Config/mysql.js'
@@ -8,7 +8,7 @@ dotenv.config()
 
 const app = express()
 
-const PORT=process.env.PORT
+const PORT = process.env.PORT
 
 app.use(express.json())
 
@@ -19,23 +19,28 @@ app.use(express.json())
 
 // Routes
 app.post("/menu", createnewItem);
-app.get("/menu",getItems)
-app.get("/menu/:id",getallItemsbyId)
-app.delete("/menu/:id",deleteItem)
-app.put("/menu/:id",updateItem)
+
+app.get("/menu", getItems)
+
+app.get("/menu/:id", getallItemsbyId)
+
+app.delete("/menu/:id", deleteItem)
+
+app.put("/menu/:id", updateItem)
+
 
 await connect_mongo_db();
 console.log('MONGO DATABASE CONNECTED FROM SERVER FILE.....');
 await Connection();
 console.log('MYSQL DATABASE CONNECTED FROM SERVER FILE.....');
 
-app.get("/",(req, res) => {
+app.get("/", (req, res) => {
     res.send("<h1> HELLO THE SERVER IS RUNNING.....</h1>")
 })
 
 app.listen(PORT, () => {
-    
-    
+
+
     console.log(`Hello The Server Is Running On PORT Number:http://localhost:${PORT}`)
     console.log('MONGO DB , MYSQL , EXPRESS.JS  EVERTHING CONNECTED WOOHOOOO............')
 })
