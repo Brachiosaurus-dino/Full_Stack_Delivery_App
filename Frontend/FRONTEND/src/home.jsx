50d390c8cdf89264156e21585ea6f3667a92c165
mantiss <manbirss9029@gmail.com>
2025-10-20 23:43:57 +0530

    doness

--- Frontend/FRONTEND/src/home.jsx
+++ Frontend/FRONTEND/src/home.jsx
@@ -1,6 +1,55 @@
 function Home() {
 
+    const boxes = [
+
+        {
+            id: 1,
+            src: "../public/anh-nguyen-kcA-c3f_3FE-unsplash.jpg",
+            caption1: "Balla Vista",
+            caption2: "Itallian Pizza Pasta",
+            time: "25-30 mins"
+        },
+        {
+            id: 2,
+            src: "../public/brooke-lark-oaz0raysASk-unsplash.jpg",
+            caption1: "The Burger House",
+            caption2: "American Burger Fast Food",
+            time: "20-30 mins"
+        },
+        {
+            id: 3,
+            src: "../public/dan-gold-4_jhDO54BYg-unsplash.jpg",
+            caption1: "Sakura Sushi",
+            caption2: "japanese Sushi",
+            time: "30-40 mins"
+        },
+        {
+            id: 4,
+            src: "../public/eaters-collective-12eHC6FxPyg-unsplash.jpg",
+            caption1: "Green Garden",
+            caption2: "healthy Slalads",
+            time: "15-25 mins"
+        },
+        {
+            id: 5,
+            src: "../public/lily-banse--YHSwy6uqvk-unsplash(1).jpg",
+            caption1: "Spice Route",
+            caption2: "Indian Spicy Curry",
+            time: "20-40 mins"
+        },
+        {
+            id: 6,
+            src: "../public/thought-catalog-9aOswReDKPo-unsplash.jpg",
+            caption1: "Taco Festa",
+            caption2: "Mexican Taco",
+            time: "23-44 mins"
+        },
+
+
+    ]
+
     const itemss = [
+
         {
             id: 1,
             src: "../public/burger.png",
@@ -73,7 +122,16 @@ function Home() {
                             ))}
                         </div>
                     </div>
+                </div>
 
+                <div className="container w-1/2 mx-auto align-middle">
+                    <div className="flex justify-between items-center ">
+                        <h3 className="text-2xl font-bold">Popular Restaurents </h3>
+                        <h3 className="text-1xl text-orange-500">View All</h3>
+                    </div>
+                    <div>
+
+                    </div>
 
                 </div>
             </div>
