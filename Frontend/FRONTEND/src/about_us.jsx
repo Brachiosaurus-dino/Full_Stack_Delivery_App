8657e279f426288a400774b1f12a12d16ee7c184
mantiss <maniidon7890@gmail.com>
2025-10-24 16:49:02 +0530

    about_us_under_progresss

---
+++ Frontend/FRONTEND/src/about_us.jsx
@@ -0,0 +1,20 @@
+function About_Us(){
+    return(
+        <>
+        <div>
+            <div className="h-full w-full bg-red-50 flex flex-col items-center py-40 ">
+                <div className="texr-center space-y-6 max-w-3xl ">
+                    <p className="font-semibold text-3xl text-center">About QuickBite</p>
+                    <p className="text-xs font-light text-center">We are on a mission bring delicious food  from your favourite local restaurent straight
+                        from local restaurent to your doorsteps faster and fresher then ever before
+                    </p>
+                </div>
+                
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default About_Us
+
