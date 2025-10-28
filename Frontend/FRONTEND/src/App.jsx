d422321e48231c60ebe8d6f1d540fd21e9584790
mantiss <maniidon7890@gmail.com>
2025-10-28 17:53:41 +0530

    contactsss

--- Frontend/FRONTEND/src/App.jsx
+++ Frontend/FRONTEND/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/navabr";
 import Home from "./home";
 import About_us from "./about_us"
 import Footer from "./components/footer";
+import Contact_Us from "./contact_us";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about_us" element={<About_us/>}/>
+        <Route path='/contact_us' element={<Contact_Us/>}/>
       </Routes>
       <Footer/>
 
---
+++ Frontend/FRONTEND/src/contact_us.jsx
@@ -0,0 +1,12 @@
+function Contact_Us(){
+    return(
+        <>
+        <div>
+                <h1 className="text-center">HEllo</h1>
+        </div>
+        </>
+    )
+}
+
+export default Contact_Us
+
