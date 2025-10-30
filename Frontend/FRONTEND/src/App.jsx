89ce198f070bb25772429d8bee17a4833dd94fdc
mantiss <mantiss2345@gmail.com>
2025-10-30 07:43:46 +0530

    fronted connected to backedend succesfully

--- Frontend/FRONTEND/src/App.jsx
+++ Frontend/FRONTEND/src/App.jsx
@@ -4,6 +4,7 @@ import About_us from "./about_us"
 import Footer from "./components/footer";
 import Contact_Us from "./contact_us";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Menu from "./menu";
 function App() {
   return (
     <>
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about_us" element={<About_us/>}/>
         <Route path='/contact_us' element={<Contact_Us/>}/>
+        <Route path='/restaurents' element={<Menu/>}/>
       </Routes>
       <Footer/>
 
