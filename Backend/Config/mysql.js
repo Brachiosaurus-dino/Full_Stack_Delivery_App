169b11a6abdd809f4d80c59009c3591c136922e2
mantiss <manbirss9029@gmail.com>
2025-11-20 21:53:23 +0530

    sql_ready

--- Backend/Config/mysql.js
+++ Backend/Config/mysql.js
@@ -18,8 +18,12 @@ export const Seq = new Sequelize(
 export const Connection = async () => {
   try {
     await Seq.authenticate()
-    const [rows] = await Seq.query('SELECT * FROM users;');
+    const [rows] = await Seq.query('SELECT * FROM customers;');
+     const [prods] = await Seq.query('SELECT * FROM products;');
+     console.log('Customers')
     console.log(rows)
+    console.log('Products')
+    console.log(prods)
     console.log("My SQL CONNECTION SUCCESSFUL FROM MYSQL FILE..........")
   }
   catch {
